6e64a8f2db4cb16d92742110d023649ef5623f5d
saif <saif.haq99@gmail.com>
2024-07-31 21:00:31 +0100

    Pushed FixedQParams

--- qconfigs.py
+++ qconfigs.py
@@ -1,6 +1,6 @@
 import torch
 import torch.quantization as tq
-from torch.ao.quantization.fake_quantize import FakeQuantize
+from torch.ao.quantization.fake_quantize import FakeQuantize, FixedQParamsFakeQuantize
 from torch.ao.quantization._learnable_fake_quantize import (
     _LearnableFakeQuantize as LearnableFakeQuantize,
 )
@@ -24,6 +24,18 @@ learnable_act = lambda range : LearnableFakeQuantize.with_args(
     zero_point=0.0,
     use_grad_scaling=True,
 )
+
+fixed_0255 = FixedQParamsFakeQuantize.with_args(
+    observer=torch.ao.quantization.observer.FixedQParamsObserver.with_args(
+        scale=1.0 / 255.0,
+        zero_point=0.0,
+        quant_min=0,
+        quant_max=255,
+        dtype=torch.quint8,
+        qscheme=torch.per_tensor_affine,
+    )
+)
+
 
 learnable_weights = lambda channels : LearnableFakeQuantize.with_args(  # need to specify number of channels here
     observer=tq.PerChannelMinMaxObserver,
