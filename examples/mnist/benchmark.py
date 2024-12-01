95e1306cdccbd32c03c89cdad3dfe324e97f309a
oscar.savolainen <oscar.savolainen@thrivelearning.com>
2024-12-01 20:29:29 +0000

    changed the inputs of benchmark_model so that one doesn't have to, but can, provide a dataloader. One can merely provide a tensor, and the benchmarking with synthesize a data loader, from the tensor shape, that it will use to benchmark the model.

--- examples/mnist/benchmark.py
+++ examples/mnist/benchmark.py
@@ -9,10 +9,11 @@ from utils.data.loaders import CircularDataLoader
 from utils.data.transforms import InferenceTransform
 
 from alma.arguments.benchmark_args import parse_benchmark_args
-from alma.benchmark.benchmark_model import benchmark_model
+from alma.benchmark_model import benchmark_model
 from alma.utils.ipdb_hook import ipdb_sys_excepthook
 from alma.utils.load_model import load_model
 from alma.utils.setup_logging import setup_logging
+from alma.utils.data import get_sample_data
 
 # One needs to set their quantization backend engine to what is appropriate for their system.
 # torch.backends.quantized.engine = 'x86'
@@ -27,6 +28,7 @@ def main() -> None:
         img_dir=args.data_dir, transform=InferenceTransform
     )
     data_loader = CircularDataLoader(dataset, batch_size=args.batch_size, shuffle=False)
+    data = get_sample_data(data_loader, device)
 
     # Load model
     load_start_time = time.perf_counter()
@@ -34,13 +36,19 @@ def main() -> None:
     load_end_time = time.perf_counter()
     logging.info(f"Model loading time: {load_end_time - load_start_time:.4f} seconds")
 
-    # Benchmark the model
-    benchmark_model(model, device, data_loader, args.n_samples, args, logging)
+    # Benchmark the model. This will generate a dataloader that provides random tensors of the
+    # same shape as `data`, which is used to benchmark the model.
+    logging.info("Benchmarking model using random data")
+    benchmark_model(model, device, data[0, :, :, :].squeeze(), args, logging)
+
+    # Benchmark the model using the provided data loader.
+    logging.info("Benchmarking model using provided data loader")
+    benchmark_model(model, device, data, args, logging, data_loader=data_loader)
 
 
 if __name__ == "__main__":
     # Adds an ipdb hook to the sys.excepthook, which will throw one into an ipdb shell when an
     # exception is raised
-    # ipdb_sys_excepthook()
+    ipdb_sys_excepthook()
     setup_logging()
     main()
