15bb9431c3550da8e44c5da2594d530035a6d0af
oscar.savolainen <oscar.savolainen@thrivelearning.com>
2024-12-03 19:30:29 +0000

    bug fixes

--- src/alma/arguments/benchmark_args.py
+++ src/alma/arguments/benchmark_args.py
@@ -68,7 +68,7 @@ def parse_benchmark_args() -> Tuple[argparse.Namespace, torch.device]:
         help="disables MPS acceleration",
     )
     parser.add_argument(
-        "--conversion",
+        "--conversions",
         type=list_of_strings,
         # choices=[str(i) for i in MODEL_CONVERSION_OPTIONS.keys()] + list(MODEL_CONVERSION_OPTIONS.values()),
         default=None,
@@ -101,31 +101,24 @@ to different transforms, or their string names. MUltiple options can be selected
     error_msg = (
         lambda conversion: f"Please select a valid option for the model conversion, {conversion} not in {valid_conversion_options}. Call `-h` for help."
     )
+    # Convert all selected conversion options to a list of strings. I.e., all ints become strings
+    # We also check that the provided conversion options are valid
+    selected_conversions = []
     for conversion in conversions:
         if isinstance(conversion, str) and conversion.isnumeric():
             conversion = int(conversion)
             assert conversion in valid_conversion_options, error_msg(conversion)
-            logger.info(
-                f"{MODEL_CONVERSION_OPTIONS[conversion]} model selected for benchmarking"
-            )
+            selected_conversions.append(MODEL_CONVERSION_OPTIONS[conversion])
         elif conversion in valid_conversion_options:
-            logger.info(f"{conversion} model selected for benchmarking")
+            selected_conversions.append(conversion)
         else:
             raise ValueError(error_msg(conversion))
 
-    # Convert all selected conversion options to a list of strings. I.e., all ints become strings
-    new_conversions = []
-    for conversion in conversions:
-        if isinstance(conversion, str) and conversion.isnumeric():
-            conversion = int(conversion)
-        if isinstance(conversion, int):
-            if MODEL_CONVERSION_OPTIONS[conversion] not in new_conversions:
-                conversions.append(MODEL_CONVERSION_OPTIONS[conversion])
-        else:
-            if conversion not in new_conversions:
-                new_conversions.append(conversion)
+    # Convert the list of selected conversions to a "pretty" string for logging
+    format_list = lambda lst: ", ".join(lst[:-1]) + (" and " + lst[-1] if len(lst) > 1 else lst[0] if lst else "")
+    logger.info(f"{format_list(selected_conversions)} model conversions selected for benchmarking\n")
 
-    args.conversions = new_conversions
+    args.conversions = selected_conversions
 
     if use_cuda:
         device = torch.device("cuda")
